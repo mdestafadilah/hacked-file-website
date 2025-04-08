332e8a4e01bd1ba6c08547e2714b1370bd69114b
mDestaFadilah <desta.08b@gmail.com>
2025-04-08 11:54:37 +0700

    Add files via upload

---
+++ index_hacker.php
@@ -0,0 +1,10 @@
+<!-- <html>
+<head>
+<title>Hacked By Farel07</title>
+<meta name="description" content="NineSec Team">
+<iframe width="0" height="0" allow="autoplay" src="https://g.top4top.io/m_20299h9zq0.mp3" frameborder="0" allowfullscreen></iframe>
+</head>
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><body style="background:black;color:white"><center><div class="four-zero-four-container"><div class="code"><div class="four-zero-four-container"><div class="code"><font size="6" style="color:white;font-family:Montserrat;"><img src="https://i.imgur.com/msWDsAR.gif" height="300"> <br><br>Hacked By Farel07</font></div><div class="message"><br><font face="Montserrat" size="5" color="white">NineSec Team &copy 2k22</div></font></center></body></head></html> -->
+
+<img src="uc.jpg"></img>
